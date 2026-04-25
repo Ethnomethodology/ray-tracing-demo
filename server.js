8a1093446a2f32f8d2214bc313b5ff955fe0428a
Dipanjan Saha <dipanjansaha@hotmail.com>
2026-04-25 09:51:51 +0100

    Add explicit routes for part2 and part3

--- server.js
+++ server.js
@@ -6,9 +6,17 @@ const PORT = process.env.PORT || 8000;
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public'), { extensions: ['html'] }));
 
-// Simple route for the home page
+// Explicit routes for clean URLs
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/part2', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'part2.html'));
+});
+
+app.get('/part3', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'part3.html'));
 });
 
 app.listen(PORT, '0.0.0.0', () => {
